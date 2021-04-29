3fe5edf177253db33a665103d8dfbb34f575dc7e
C05_Basem <basemhmoud735@gmail.com>
2021-04-30 02:13:06 +0300

    add arrlaptop containg obj in main.js and add div have id l1

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log(arrowTitle)
+// /* START CODE UNDER THIS LINE */
+
+const arrLaptop = [
+  {
+    name: "toshipa",
+    img: "./toshipa.png",
+  },
+  {
+    name: "php",
+    img: "./php.jpg",
+  },
+];
+
+const l1=$("#l1")
+
