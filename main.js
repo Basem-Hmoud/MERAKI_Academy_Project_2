3f1f623c16682df2ed390033a7a497c03a7826e9
C05_Basem <basemhmoud735@gmail.com>
2021-04-30 02:41:35 +0300

    add some comment to solve proplem in main.js

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ const arrLaptop = [
 
 const l1=$("#l1")
 
+
+//for loop in arrLaptop
+//arrLaptop
+//for(let i =0;i<arrLaptop.length,i++){
+// dynamic add name and img in arrLaptop{
+  //using append to view content obj body.append(paragraph)
+//}
+
+
+
+//show / hide
+//the first show tosipa and hide php using onclick=click
+// on hover change the color to red
+/*
+//replace mouseover > click
+listItems.on("mouseover", function () {
+  // $(this) refers to the element it self, to have the correct reference use ES5
+  $(this).css("color", "red");
+  console.log("color has been changed");
+});*/
+//.show() header.show();
+//.hide() header.hide();
