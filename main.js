49a293c5fa226b356fcfdb9b46f68f204f95f7ae
C05_Basem <basemhmoud735@gmail.com>
2021-04-30 03:13:38 +0300

    rename array from arrLaptop to arr and add in array and #l2 and some comment

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
 // console.log(arrowTitle)
 // /* START CODE UNDER THIS LINE */
 
-const arrLaptop = [
+const arr = [
   {
     name: "toshipa",
     img: "./toshipa.png",
+    type: "laptop",
   },
   {
     name: "php",
     img: "./php.jpg",
+    type: "laptop",
+  },
+  {
+    name: "acer",
+    img: "./acer.jpg",
+    type: "laptop",
+  },
+  {
+    name: "Dell",
+    img: "./dell.jpg",
+    type: "laptop",
   },
 ];
 
-const l1=$("#l1")
+const l1 = $("#l1");
+const l2 = $("#l2");
 
-
-//for loop in arrLaptop
-//arrLaptop
-//for(let i =0;i<arrLaptop.length,i++){
-// dynamic add name and img in arrLaptop{
-  //using append to view content obj body.append(paragraph)
+//for loop in arr
+//arr
+//for(let i =0;i<arr.length,i++){
+// dynamic add name and img in arr{
+//using append to view content obj body.append(paragraph)
 //}
-
-
 
 //show / hide
 //the first show tosipa and hide php using onclick=click
 // on hover change the color to red
 /*
+
+
 //replace mouseover > click
 listItems.on("mouseover", function () {
   // $(this) refers to the element it self, to have the correct reference use ES5
