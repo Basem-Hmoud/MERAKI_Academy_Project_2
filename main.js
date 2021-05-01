3db95be59f0109308339812d8f48f827434dadeb
C05_Basem <basemhmoud735@gmail.com>
2021-05-01 14:33:51 +0300

    update virable name

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // console.log(arrowTitle)
 // /* START CODE UNDER THIS LINE */
 
-const arr = [
+const arrayItems = [
   {
     //////////////////////////////laptop
     name: "toshipa",
@@ -150,135 +150,135 @@ const arr = [
   },
 ];
 
-const l1 = $("#l1"); //لا ظهار محتويات اللاب توب
-const l2 = $("#l2"); //لاظهار محتويات التلفونات
-const l3 = $("#l3"); //لاظهار محتويات اكسسوارات اللاب توب
-const l4 = $("#l4"); //لاظهار محتويات افلام
-const l5 = $("#l5"); //لاطهار عنصر واحد عند الضغط على زر التفاصيل
+const section1 = $("#section1"); //لا ظهار محتويات اللاب توب
+const section2 = $("#section2"); //لاظهار محتويات التلفونات
+const section3 = $("#section3"); //لاظهار محتويات اكسسوارات اللاب توب
+const section4 = $("#section4"); //لاظهار محتويات افلام
+const section5 = $("#section5"); //لاطهار عنصر واحد عند الضغط على زر التفاصيل
 
-const p1 = $("#p1"); //لا ظهار محتويات اللاب توب
-const p2 = $("#p2"); //لاظهار محتويات التلفونات
-const p3 = $("#p3"); //لاظهار محتويات اكسسوارات اللاب توب
-const p4 = $("#p4"); //لاظهار محتويات افلام
+const paragraph1 = $("#paragraph1"); //لا ظهار محتويات اللاب توب
+const paragraph2 = $("#paragraph2"); //لاظهار محتويات التلفونات
+const paragraph3 = $("#paragraph3"); //لاظهار محتويات اكسسوارات اللاب توب
+const paragraph4 = $("#paragraph4"); //لاظهار محتويات افلام
 // view all content in laptop
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arrayItems.length; i++) {
   //return all obj type laptop name + img
-  if (arr[i].type === "laptop") {
-    l1.append(`
+  if (arrayItems[i].type === "laptop") {
+    section1.append(`
     <div>
-    <h2>${arr[i].name}</h2><br/>
-    <img src="${arr[i].img}"/><br/><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <img src="${arrayItems[i].img}"/><br/><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     <button id="deslaptop${i}"> details</button>
     </div>`);
     let detaillaptop = $(`#deslaptop${i}`);
     detaillaptop.on("click", () => {
-    //to view 1 items
-    l5.append(`
-    <div><img src="${arr[i].img}"/></div>
+      //to view 1 items
+      section5.append(`
+    <div><img src="${arrayItems[i].img}"style= "width: auto;"/></div>
     <div class="as">
-    <h2>${arr[i].name}</h2><br/>
-    <h3>${arr[i].description}</h3><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <h3>${arrayItems[i].description}</h3><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     </div>`);
 
-      l1.hide();
-      p1.hide();
-      l2.hide();
-      p2.hide();
-      l3.hide();
-      p3.hide();
-      l4.hide();
-      p4.hide();
-      l5.show();
+      section1.hide();
+      paragraph1.hide();
+      section2.hide();
+      paragraph2.hide();
+      section3.hide();
+      paragraph3.hide();
+      section4.hide();
+      paragraph4.hide();
+      section5.show();
     });
   }
 }
 // view all content in phone
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arrayItems.length; i++) {
   //return all obj type phone name + img
-  if (arr[i].type === "phone") {
-    l2.append(`
+  if (arrayItems[i].type === "phone") {
+    section2.append(`
     <div>
-    <h2>${arr[i].name}</h2><br/>
-    <img src="${arr[i].img}" style= "height:300px ;"/><br/><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <img src="${arrayItems[i].img}" style= "height:300px ;"/><br/><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     <button id="desPhone${i}"> details</button>
     </div>`);
     let detailPhone = $(`#desPhone${i}`);
     detailPhone.on("click", () => {
-    //to view 1 items
-    l5.append(`
+      //to view 1 items
+      section5.append(`
     <div>
-    <img src="${arr[i].img}"/></div>
+    <img src="${arrayItems[i].img}"style= "width: auto;"/></div>
     <div class="as">
-    <h2>${arr[i].name}</h2><br/>
-    <h3>${arr[i].description}</h3><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <h3>${arrayItems[i].description}</h3><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     </div>`);
 
-      l1.hide();
-      p1.hide();
-      l2.hide();
-      p2.hide();
-      l3.hide();
-      p3.hide();
-      l4.hide();
-      p4.hide();
-      l5.show();
+      section1.hide();
+      paragraph1.hide();
+      section2.hide();
+      paragraph2.hide();
+      section3.hide();
+      paragraph3.hide();
+      section4.hide();
+      paragraph4.hide();
+      section5.show();
     });
   }
 }
 // view all content in Accessories laptop
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arrayItems.length; i++) {
   //return all obj type Accessories laptop name + img
-  if (arr[i].type === "Accessories laptop") {
-    l3.append(`
+  if (arrayItems[i].type === "Accessories laptop") {
+    section3.append(`
     <div>
-    <h2>${arr[i].name}</h2><br/>
-    <img src="${arr[i].img}" style= "height:300px ;width:300px;"/><br/><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <img src="${arrayItems[i].img}" style= "height:300px ;width:300px;"/><br/><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     <button id="desAcc${i}"> details</button>
     </div>`);
     let detailAcc = $(`#desAcc${i}`);
     detailAcc.on("click", () => {
-    //to view 1 items
-    l5.append(`
+      //to view 1 items
+      section5.append(`
     <div>
-    <img src="${arr[i].img}"/></div>
+    <img src="${arrayItems[i].img}"style= "width: auto;"/></div>
     <div class="as">
-    <h2>${arr[i].name}</h2><br/>
-    <h3>${arr[i].description}</h3><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <h3>${arrayItems[i].description}</h3><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     </div>`);
 
-      l1.hide();
-      p1.hide();
-      l2.hide();
-      p2.hide();
-      l3.hide();
-      p3.hide();
-      l4.hide();
-      p4.hide();
-      l5.show();
+      section1.hide();
+      paragraph1.hide();
+      section2.hide();
+      paragraph2.hide();
+      section3.hide();
+      paragraph3.hide();
+      section4.hide();
+      paragraph4.hide();
+      section5.show();
     });
   }
 }
 // view all content in movie
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arrayItems.length; i++) {
   //return all obj type movie laptop name + img
-  if (arr[i].type === "movie") {
-    l4.append(`
+  if (arrayItems[i].type === "movie") {
+    section4.append(`
     <div style="padding-bottom: 50px;">
-    <h2>${arr[i].name}</h2><br/>
-    <img src="${arr[i].img}"/><br/><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <img src="${arrayItems[i].img}"/><br/><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     <button id="desMov${i}"> details</button>
     </div>`);
@@ -288,102 +288,103 @@ for (let i = 0; i < arr.length; i++) {
       console.log("hi every body");
 
       //to view 1 items
-      l5.append(`
+      section5.append(`
     <div>
-    <img src="${arr[i].img}"/></div>
+    <img src="${arrayItems[i].img}"style= "width: auto;"/></div>
     <div class="as">
-    <h2>${arr[i].name}</h2><br/>
-    <h3>${arr[i].description}</h3><br/>
-    <h4>${arr[i].price}</h4><br/>
+    <h2>${arrayItems[i].name}</h2><br/>
+    <h3>${arrayItems[i].description}</h3><br/>
+    <h4>${arrayItems[i].price}</h4><br/>
     <button class="buy"> add to card</button>
     </div>`);
 
-      l1.hide();
-      p1.hide();
-      l2.hide();
-      p2.hide();
-      l3.hide();
-      p3.hide();
-      l4.hide();
-      p4.hide();
-      l5.show();
+      section1.hide();
+      paragraph1.hide();
+      section2.hide();
+      paragraph2.hide();
+      section3.hide();
+      paragraph3.hide();
+      section4.hide();
+      paragraph4.hide();
+      section4.hide();
+      section5.show();
     });
   }
 }
 // selecting the elements with the `listItem` class
-const lap1 = $("#lap1");
-const pho1 = $("#pho1");
-const AccLab = $("#AccLab");
-const mov = $("#mov");
-const home = $("#home");
-// if click in lap1 show all content laptope and Hide the rest of the sections
-lap1.on("click", () => {
-  l1.show();
-  p1.show();
-  l2.hide();
-  p2.hide();
-  l3.hide();
-  p3.hide();
-  l4.hide();
-  p4.hide();
-  l5.hide();
+const laptopPage = $("#laptopPage");
+const phonePage = $("#phonePage");
+const accessoriesPage = $("#accessoriesPage");
+const moviePage = $("#moviePage");
+const homePage = $("#homePage");
+// if click in laptopPage show all content laptope and Hide the rest of the sections
+laptopPage.on("click", () => {
+  section1.show();
+  paragraph1.show();
+  section2.hide();
+  paragraph2.hide();
+  section3.hide();
+  paragraph3.hide();
+  section4.hide();
+  paragraph4.hide();
+  section5.hide();
   //we added because every click in btn details return more items
-  l5.html("");
+  section5.html("");
 });
 // if click in phone show all content phone and Hide the rest of the sections
-pho1.on("click", () => {
-  l2.show();
-  p2.show();
-  l1.hide();
-  p1.hide();
-  l3.hide();
-  p3.hide();
-  l4.hide();
-  p4.hide();
-  l5.hide();
-  l5.html("");
+phonePage.on("click", () => {
+  section2.show();
+  paragraph2.show();
+  section1.hide();
+  paragraph1.hide();
+  section3.hide();
+  paragraph3.hide();
+  section4.hide();
+  paragraph4.hide();
+  section5.hide();
+  section5.html("");
 });
 // if click in Accessories laptop show all content Accessories laptop and Hide the rest of the sections
-AccLab.on("click", () => {
-  l3.show();
-  p3.show();
-  l1.hide();
-  p1.hide();
-  l2.hide();
-  p2.hide();
-  l4.hide();
-  p4.hide();
-  l5.hide();
-  l5.html("");
+accessoriesPage.on("click", () => {
+  section3.show();
+  paragraph3.show();
+  section1.hide();
+  paragraph1.hide();
+  section2.hide();
+  paragraph2.hide();
+  section4.hide();
+  paragraph4.hide();
+  section5.hide();
+  section5.html("");
 });
 // if click in movie show all content movie and Hide the rest of the sections
-mov.on("click", () => {
-  l4.show();
-  p4.show();
-  l1.hide();
-  p1.hide();
-  l2.hide();
-  p2.hide();
-  l3.hide();
-  p3.hide();
-  l5.hide();
-  l5.html("");
+moviePage.on("click", () => {
+  section4.show();
+  paragraph4.show();
+  section1.hide();
+  paragraph1.hide();
+  section2.hide();
+  paragraph2.hide();
+  section3.hide();
+  paragraph3.hide();
+  section5.hide();
+  section5.html("");
 });
 // if click in home show all department
-home.on("click", () => {
-  l1.show();
-  p1.show();
-  l2.show();
-  p2.show();
-  l3.show();
-  p3.show();
-  l4.show();
-  p4.show();
-  l5.hide();
-  l5.html("");
+homePage.on("click", () => {
+  section1.show();
+  paragraph1.show();
+  section2.show();
+  paragraph2.show();
+  section3.show();
+  paragraph3.show();
+  section4.show();
+  paragraph4.show();
+  section5.hide();
+  section5.html("");
 });
 //if click on add to card and show alert
-const card = $(".buy");
-card.on("click", () => {
+const viewOneItem = $(".buy");
+viewOneItem.on("click", () => {
   alert("thank you for buying");
 });
